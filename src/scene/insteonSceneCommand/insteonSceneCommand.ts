430b0a9e07f5fbc35a06dcaa96c0a71103be58e8
Andrew Pearson <andrew@apearson.io>
2019-11-28 01:08:37 +0000

    Code cleanup

--- src/scene/insteonSceneCommand/insteonSceneCommand.ts
+++ src/scene/insteonSceneCommand/insteonSceneCommand.ts
@@ -1,7 +1,6 @@
 import { Red, NodeProperties } from 'node-red';
-import { Packet } from 'insteon-plm';
 import { InsteonSceneConfigNode, SceneCommandNode } from '../../types/types';
-
+import PowerLincModem, { Byte } from 'insteon-plm';
 
 interface SceneSubscribeConfigNodeProps extends NodeProperties {
 	scene: string;
@@ -30,20 +29,14 @@ export = function(RED: Red){
 		this.sceneConfigNode = RED.nodes.getNode(props.scene) as InsteonSceneConfigNode;
 
 		/* Setting up PLM events */
-		this.sceneConfigNode.on('packet', p => onPacket(this, p));
-		this.sceneConfigNode.on('status', p => onStatus(this, p));
 		this.sceneConfigNode.on('ready', p => onReady(this, p));
+
+		// Listening for msgs
+		this.on('input', (msg) => onInput(msg, this));
 	});
 };
 
 //#region Event Functions
-
-function onStatus(node: SceneCommandNode, text: string){
-
-	node.status({ fill: "blue", shape: 'dot', text });
-
-	node.send({topic: 'ready', payload: text});
-}
 
 function onReady(node: SceneCommandNode, text: string){
 	node.log('Ready');
@@ -53,8 +46,63 @@ function onReady(node: SceneCommandNode, text: string){
 	node.send({topic: 'ready', payload: text});
 }
 
-function onPacket(node: SceneCommandNode, packet: Packet.Packet){
-	node.send({topic: 'packet', payload: packet});
+//#endregion
+
+//#region Input Functions
+
+async function onInput(msg: any, node: SceneCommandNode){
+
+	// Grabbing PLM
+	const plm = node.sceneConfigNode?.PLMConfigNode?.plm;
+
+	// Checking we have a modem
+	if(plm === undefined){
+		node.status({ fill: 'red', shape: "dot", text: 'No Modem' });
+		return;
+	}
+
+	// Grabbing scene to command
+	const scene = node.sceneConfigNode?.scene as Byte;
+
+	// Checking we have a scene
+	if(scene === undefined){
+		node.status({ fill: 'red', shape: "dot", text: 'No scene selected' });
+		return;
+	}
+
+	// Determining what action to take
+	if(msg.topic === 'switch'){
+		await toggle(node, msg, plm, scene);
+	}
+}
+
+//#endregion
+
+//#region Commands
+
+async function toggle(node: SceneCommandNode, msg: any, plm: PowerLincModem, scene: Byte){
+
+	// Grabbing status from msg payload
+	const status = msg?.payload?.status;
+
+	// Checking status for correct format
+	if(status === undefined || (status !== 'off' && status !== 'on')){
+		node.error('Payload or Status in incorrect format');
+		return;
+	}
+
+	const turnOn = [0x11, 0xFF] as Byte[];
+	const turnOff = [0x13, 0x00] as Byte[];
+
+	// Determining bytes
+	const cmd = status.trim() === 'on'? turnOn : turnOff;
+
+	// Sending command
+	const ack = await plm.sendAllLinkCommand(scene, cmd[0], cmd[1]);
+
+	if(!ack){
+		node.log('Could not execute command');
+	}
 }
 
 //#endregion
