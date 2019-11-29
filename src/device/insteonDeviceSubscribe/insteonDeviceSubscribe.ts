0d3cb0ac5e6e08d9fb5716ad84028e3735f1e5ce
Andrew Pearson <andrew@apearson.io>
2019-11-29 23:56:27 +0000

    Device subscribe now listens for all events

--- src/device/insteonDeviceSubscribe/insteonDeviceSubscribe.ts
+++ src/device/insteonDeviceSubscribe/insteonDeviceSubscribe.ts
@@ -2,7 +2,6 @@ import { Red, NodeProperties } from 'node-red';
 import { Packet } from 'insteon-plm';
 import { DeviceSubscribeNode, InsteonDeviceConfigNode } from '../../types/types';
 
-
 interface SubscribeConfigNodeProps extends NodeProperties {
 	device: string;
 	selectedEvents: string[];
@@ -46,22 +45,26 @@ function onReady(node: DeviceSubscribeNode, text: string){
 
 	node.status({ fill: 'green', shape: 'dot', text });
 
-	node.send({topic: 'ready', payload: text});
+	node.send({ topic: 'ready', payload: text });
 
 	// Adding event listeners
 	node.deviceConfigNode?.device?.on(['packet', '**'], function (this: any, p){ onPacket(node, this.event, p) });
+
+	// Device events
 	node.deviceConfigNode?.device?.on(['switch', '**'], function (this: any, p){ onSwitch(node, this.event, p) });
-	node.deviceConfigNode?.device?.on(['dim', '**' ], function (this: any, p){ onSwitch(node, this.event, p) });
+	node.deviceConfigNode?.device?.on(['dim', '**' ], function (this: any, p){ onDim(node, this.event, p) });
+	node.deviceConfigNode?.device?.on(['sensor', '**' ], function (this: any, p){ onSensor(node, this.event, p) });
+	node.deviceConfigNode?.device?.on(['heartbeat', '**' ], function (this: any, p){ onHeartbeat(node, this.event, p) });
+	node.deviceConfigNode?.device?.on(['battery', '**' ], function (this: any, p){ onBattery(node, this.event, p) });
+	node.deviceConfigNode?.device?.on(['motion', '**' ], function (this: any, p){ onMotion(node, this.event, p) });
+	node.deviceConfigNode?.device?.on(['light', '**' ], function (this: any, p){ onLight(node, this.event, p) });
 }
 
 function onPacket(node: DeviceSubscribeNode, event: string[], packet: Packet.Packet){
-	node.log('Got packet');
-
 	node.send({topic: 'packet', payload: packet});
 }
 
 function onSwitch(node: DeviceSubscribeNode, event: string[], packet: Packet.Packet){
-
 	node.send({
 		topic: event[0],
 		payload: {
@@ -72,5 +75,72 @@ function onSwitch(node: DeviceSubscribeNode, event: string[], packet: Packet.Pac
 		packet,
 	});
 }
+
+function onDim(node: DeviceSubscribeNode, event: string[], packet: Packet.Packet){
+	node.send({
+		topic: event[0],
+		payload: {
+			status: event[1],
+		},
+		event,
+		packet,
+	});
+}
+
+function onSensor(node: DeviceSubscribeNode, event: string[], packet: Packet.Packet){
+	node.send({
+		topic: event[0],
+		payload: {
+			status: event[1],
+		},
+		event,
+		packet,
+	})
+}
+
+function onHeartbeat(node: DeviceSubscribeNode, event: string[], packet: Packet.Packet){
+	node.send({
+		topic: event[0],
+		payload: {
+			status: event[1],
+		},
+		event,
+		packet,
+	})
+}
+
+function onBattery(node: DeviceSubscribeNode, event: string[], packet: Packet.Packet){
+	node.send({
+		topic: event[0],
+		payload: {
+			status: event[1],
+		},
+		event,
+		packet,
+	})
+}
+
+function onMotion(node: DeviceSubscribeNode, event: string[], packet: Packet.Packet){
+	node.send({
+		topic: event[0],
+		payload: {
+			status: event[1],
+		},
+		event,
+		packet,
+	})
+}
+
+function onLight(node: DeviceSubscribeNode, event: string[], packet: Packet.Packet){
+	node.send({
+		topic: event[0],
+		payload: {
+			status: event[1],
+		},
+		event,
+		packet,
+	})
+}
+
 
 //#endregion
