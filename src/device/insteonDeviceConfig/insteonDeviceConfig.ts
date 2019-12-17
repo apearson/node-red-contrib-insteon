c765aa60edfb10617d15eed00daebea2a087145b
Paul Wieland <p@ul.sickdimension.com>
2019-12-17 16:02:42 -0500

    Cleanup imports

--- src/device/insteonDeviceConfig/insteonDeviceConfig.ts
+++ src/device/insteonDeviceConfig/insteonDeviceConfig.ts
@@ -9,14 +9,13 @@
 */
 
 /* Importing Libraries and types */
-import { Red, NodeProperties } from 'node-red';
+import { Red } from 'node-red';
 import { InsteonDeviceConfigNode } from '../../types/types';
 import { setupDevice, DeviceConfigNodeProps } from './configMethods';
 
 
 /* Exporting Node Function */
 export = function(RED: Red) {
-
 	// Registering node type and a constructor, callback function can't be async because of node red. Fired on every deploy of the node.
 	RED.nodes.registerType('insteon-device-config', function(this: InsteonDeviceConfigNode, config: DeviceConfigNodeProps) {
 		// Setting up the device
